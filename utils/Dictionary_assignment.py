535fee76a1edcbc111de93c3397696e700389bd2
Vijit Ganguly <vijitganguly1995@gmail.com>
2025-06-23 20:53:31 +0530

    commit1

--- utils/Dictionary_assignment.py
+++ utils/Dictionary_assignment.py
@@ -1,6 +1,10 @@
 dict1 = {
-    "CURVE_1": "CURVE_2", "CURVE_2": "CURVE_3", "CURVE_4": "CURVE_3", "CURVE_5": "CURVE_1",
-    "CURVE_10": "CURVE_20", "CURVE_30": "CURVE_20"}
+    "CURVE_1": "CURVE_2",
+    "CURVE_2": "CURVE_3",
+    "CURVE_4": "CURVE_3",
+    "CURVE_5": "CURVE_1",
+    "CURVE_10": "CURVE_20",
+    "CURVE_30": "CURVE_20"}
 dict_old = dict1.copy()
 
 for key, value in dict_old.items():
@@ -8,10 +12,10 @@ for key, value in dict_old.items():
 
 dict_new = {}
 
-for key in dict_old:
-    iter = dict_old[key]
-    while iter in dict_old:
-        iter = dict_old[iter]
-    dict_new[key] = iter
+for str_key in dict_old:
+    str_val = dict_old[str_key]
+    while str_val in dict_old:
+        str_val = dict_old[str_val]
+    dict_new[str_key] = str_val
 
 print(dict_new)
